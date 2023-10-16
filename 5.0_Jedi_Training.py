b1c3cac323153e861a14032a7de2c0e5ac9807d2
lamarreava <lamarreava@urbandaleschools.com>
2023-10-16 10:24:48 -0500

    commit, in the middle of coding

--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -3,44 +3,46 @@
 '''
  1. Make the following program work. LIST THE 3 MISTAKES (5pts)
    '''
+import random
+
 print("This program takes three integers and returns the sum.")
 total = 0
 for i in range(3):
-    x = input("Enter a number: ")
-    total+=i
-print("The total is:", x)
-
-#1
-#2
-#3
+    x = int(input("Enter a number: "))
+    total += x
+print("The total is:", total)
+print()
+#1 Nothing is being added to the total, change i to x
+#2 put int() in front of the input
+#3 make it print total not x
 
 
 '''
   2. Write a Python program that will use a FOR loop to print the even
      numbers from 2 to 100, inclusive. (5pts)
 '''
-
-
-
-
-
+print("Question 2")
+for i in range(2 , 102, 2):
+    print(i)
+print()
 '''
   3. Write a program that will use a WHILE loop to count from
      10 down to, and including, 0. Then print the words Blast off! Remember, use
      a WHILE loop, don't use a FOR loop. (5pts)
 '''
-
-
-
-
-
+print("Question 3")
+i = 10
+while i > -1:
+    print(i)
+    i -= 1
+print()
 
 '''
   4. Write a program that prints a random integer from 1 to 10 (inclusive). (5pts)
 '''
-
-
-
+print("Question 4")
+myNumber = random.randrange(1, 11)
+print(myNumber)
 
 
 
@@ -68,10 +70,20 @@ print("The total is:", x)
 3.) Add a loop so that the program does this 50 times.
 4.) Create a running total for the number of heads and the number of tails and print the total at the end.
 '''
-
-
-
-
+print("Question 6")
+heads = 0
+tails = 0
+for i in range(50):
+    coinToss = random.randrange(0, 2)
+    if coinToss == 0:
+        print("Heads")
+        heads += 1
+    else:
+        print("Tails")
+        tails += 1
+print("Total Count For Heads = ", heads)
+print("Total Count For Tails = ", tails)
+print()
 
 '''
 ROSHAMBO PROGRAM (15pts)
--- CAMEL_GAME_PROJECT.py
+++ CAMEL_GAME_PROJECT.py
@@ -4,12 +4,77 @@ CAMEL GAME
 The pseudo-code for how to code this game is in Chapter 5 of the Python Jedi book
 
 '''
+# PRE GAME KNOWLEDGE
+print("Camel")
+print("Welcome to Camel. The Object is to travel 200 Miles across the Great Gobi Dessert.")
+print("A tribe of jhuihiujhuhu will be chasing you.")
+print("You will be asked for commands every so often")
+print()
+print()
+print("C  O  M  M  A  N  D  S:")
+print("A. DRINK from your canteen")
+print("B. MODERATE SPEED ahead")
+print("C. FULL SPEED ahead")
+print("D. STOP for the night")
+print("E. STATUS check")
+print("Q. QUIT")
+print()
+print()
+print("You will have 1 quart of water which will last you six drinks.")
+print("You may renew your water supply completely at an oases.")
+print("Good Luck and good cameling!!")
+# end of pre game
 
+done = False
+miles_travelled = 0
+thirst = 0
+camel_tire = 0
+canteen = 6
+behind = -20
 
+while done == False:
+    print("You are in the middle of the desert at an oasis.")
+    print()
+    print("You have travelled ", miles_travelled, " miles all together")
+    print("A:DRINK B:MODERATE SPEED C:FULL SPEED D:STOP E:STATUS Q:QUIT")
+    command = input("What is your command: ")
+    print()
 
+    if command.lower() == "a":
+        print("You took a drink of water")
+    elif command.lower() == "b":
+        print("You moved ahead at moderate speed")
+    elif command.lower() == "c":
+        print(" You moved ahead at full speed")
+    elif command.lower() == "d":
+        print("You stopped for the night")
+    elif command.lower() == "e":
+        print("Status")
+    elif command.lower() == "q":
+        done = True
+        print("You Quit")
+        print()
 
+    if miles_travelled == behind:
+        pass
 
-
-
-
-
+    #End screens
+    print()
+    print()
+    print("Want to try your luck again?")
+    replay = input("1 To Replay | 2 To Stay Dead: ")
+    if replay == "1":
+            done = False
+    else:
+        done = True
+        print("<-_------______------______-----__->")
+        print(" >  HERE LIES A FOOLISH TRAVELER   <")
+        print("<__-----________----________---____>")
+        print("             _______")
+        print("            |       |")
+        print("            |  RIP  |")
+        print("            |       |")
+        print("          ''''''''''''' ")
+print()
+print("The Game Is Over")
+print("Was that fun?")
